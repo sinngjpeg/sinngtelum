fc39da64cd0c5cf8f30efd096db9cf7f865ca0af
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-19 22:44:11 -0300

    criando oncliklistener e fun publica tweet com log.i

--- app/src/main/java/com/sinngjpeg/sinngtelum/TweetActivity.kt
+++ app/src/main/java/com/sinngjpeg/sinngtelum/TweetActivity.kt
@@ -2,10 +2,21 @@ package com.sinngjpeg.sinngtelum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class TweetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweet)
+
+        val botao = findViewById<Button>(R.id.criar_tweet)
+        botao.setOnClickListener(View.OnClickListener { publicaTweet }))
+
+        private fun publicaTweet() {
+            Log.i("Tweet", "botao clicado")
+        }
+
     }
 }
